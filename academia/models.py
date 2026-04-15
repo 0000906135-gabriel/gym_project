6364b921bba37f80082a97e7d316b9b5932939f0
bernardoeloy <0001157740@senaimgaluno.com.br>
2026-04-15 18:43:29 -0300

    Criação dos models Aluno, Exercicio e Treino

--- academia/models.py
+++ academia/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Exercicio(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Treino(models.Model):
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    exercicio = models.ForeignKey(Exercicio, on_delete=models.CASCADE)
+    series = models.IntegerField()
+    repeticoes = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.aluno.nome} - {self.exercicio.nome}"
